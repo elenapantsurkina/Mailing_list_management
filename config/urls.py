eff56a9b32815aaff671c89c41e41ee694477d8f
elena_pantsurkina <pantsurkina.e@bk.ru>
2024-12-02 19:22:09 +0700

    восстановление пароля

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('users/', include('django.contrib.auth.urls')),
     path(
         "",
         include(
--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,33 @@
 from django.urls import path
 from users.apps import UsersConfig
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (LoginView, LogoutView, PasswordResetView, PasswordResetDoneView,
+                                       PasswordResetConfirmView, PasswordResetCompleteView)
 from users.views import UserCreateView, email_verification
+from django.urls import reverse_lazy
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path("login/", LoginView.as_view(template_name="login.html"), name="login"),
     path("logout/", LogoutView.as_view(template_name="logout.html"), name="logout"),
+    path("password-reset/",
+         PasswordResetView.as_view(
+             template_name="users/password_reset_form.html",
+             email_template_name="users/password_reset_email.html",
+             success_url=reverse_lazy("users:password_reset_done")
+         ),
+         name="password_reset"),
+    path("password-reset/done", PasswordResetDoneView.as_view(template_name="users/password_reset_done.html"),
+         name="password_reset_done"),
+    path("password-reset/<uib64>/<token>/",
+         PasswordResetConfirmView.as_view(
+             template_name="users/password_reset_confirm.html",
+             success_url=reverse_lazy("users:password_reset_complete")
+         ),
+         name="password_reset_confirm"),
+    path("password-reset/complete/",
+         PasswordResetCompleteView.as_view(template_name="users/password_reset_complete.html"),
+         name="password_reset_complete"),
     path("register/", UserCreateView.as_view(), name="register"),
     path("email-confirm/<str:token>/", email_verification, name="email-confirm"),
 
